03123278a442e799eceaf2f7e8ef6515a82c0c7c
Igor <igolopolosov@gmail.com>
2016-09-22 01:48:32 +0200

    Добавлен реакт роутер

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {browserHistory, Router, Route, IndexRoute, Redirect} from "react-router";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let routes = (
+    <Route path="/" component={App} />
+);
+
+ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById('root'));
