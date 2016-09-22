f3870310d0098c86763ae18787084ab761020231
Igor <igolopolosov@gmail.com>
2016-09-23 01:44:21 +0200

    central point to map

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@ require('../styles/index.scss');
 export default class App extends Component {
   render() {
     return (
-        <form className="main-form" action="http://localhost:9090/csvtodocx" method="post" encType="multipart/form-data" style={{textAlign:"center"}}>
+        <form className="main-form" action="http://localhost:9090/map" method="post" encType="multipart/form-data" style={{textAlign:"center"}}>
           <span>DOCX: </span><br/>
           <input type="file" name="tpl" accept="docx"/><br/>
           <span>CSV: </span><br/>
