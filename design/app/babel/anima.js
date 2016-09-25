486790fea31c593e73d38ef13f929e6888a923bf
debudda <db.debudda.db@gmail.com>
2016-09-25 13:15:30 +0200

    Add send button

--- design/app/babel/anima.js
+++ design/app/babel/anima.js
@@ -1,10 +1,10 @@
-// attention catcher
-
-let tlpNot = document.querySelector('.tlpNot'),
-    dictNot = document.querySelector('.dictNot');
-
-let tlpNotBlink = setInterval( function() {tlpNot.classList.toggle('toBlink')}, 1000);
-let dictNotBlink = setInterval( function() {dictNot.classList.toggle('toBlink')}, 1000);
+// // attention catcher
+//
+// let tlpNot = document.querySelector('.tlpNot'),
+//     dictNot = document.querySelector('.dictNot');
+//
+// let tlpNotBlink = setInterval( function() {tlpNot.classList.toggle('toBlink')}, 1000);
+// let dictNotBlink = setInterval( function() {dictNot.classList.toggle('toBlink')}, 1000);
 
 // uploadAnimation
 let upload = () => {
@@ -24,55 +24,55 @@ let count = 0;
   }, 800);
 }
 
-// menu change
-document.getElementById('try').focus();
-
-let menuContents = {
-  try: document.querySelector('.dropCircle'),
-  about: document.querySelector('.aboutScreen'),
-  faq: document.querySelector('faqScreen')
-}
-
-let inFocus = {
-  try: true,
-  about: false,
-  faq: false
-}
-
-let replaceClass = (element, it, withIt, addAfter300) => {
-  element.className = element.className.replace(it, withIt);
-  setTimeout( () => {
-    addClass(element, ` ${addAfter300}`);// написать тренарный иф
-  }, 300);
-}
-
-let addClass = (element, it) => {
-  element.className = element.className + it;
-}
-
-let changeScreen = id => {
-
-  for (let id in inFocus) {
-    if (inFocus[id] === id) {
-      return;
-    } else if (inFocus[id]) {
-      inFocus[id] = false;
-      replaceClass(menuContents[id], ' display', ' fadeOutLeft',' displayNone');
-    }
-  }
-
-  if (id === 'try') {
-
-    replaceClass(menuContents[id], ' displayNone', '', 'display');
-    addClass(menuContents[id], ' fadeInLeft');
-    inFocus[id] = true;
-
-  } else {
-
-    replaceClass(menuContents[id], ' displayNone', '', 'display');
-    addClass(menuContents[id], ' fadeInRight');
-    inFocus[id] = true;
-
-  }
-
-}
+// // menu change
+// document.getElementById('try').focus();
+//
+// let menuContents = {
+//   try: document.querySelector('.dropCircle'),
+//   about: document.querySelector('.aboutScreen'),
+//   faq: document.querySelector('faqScreen')
+// }
+//
+// let inFocus = {
+//   try: true,
+//   about: false,
+//   faq: false
+// }
+//
+// let replaceClass = (element, it, withIt, addAfter300) => {
+//   element.className = element.className.replace(it, withIt);
+//   setTimeout( () => {
+//     addClass(element, ` ${addAfter300}`);// написать тренарный иф
+//   }, 300);
+// }
+//
+// let addClass = (element, it) => {
+//   element.className = element.className + it;
+// }
+//
+// let changeScreen = id => {
+//
+//   for (let id in inFocus) {
+//     if (inFocus[id] === id) {
+//       return;
+//     } else if (inFocus[id]) {
+//       inFocus[id] = false;
+//       replaceClass(menuContents[id], ' display', ' fadeOutLeft',' displayNone');
+//     }
+//   }
+//
+//   if (id === 'try') {
+//
+//     replaceClass(menuContents[id], ' displayNone', '', 'display');
+//     addClass(menuContents[id], ' fadeInLeft');
+//     inFocus[id] = true;
+//
+//   } else {
+//
+//     replaceClass(menuContents[id], ' displayNone', '', 'display');
+//     addClass(menuContents[id], ' fadeInRight');
+//     inFocus[id] = true;
+//
+//   }
+//
+// }
